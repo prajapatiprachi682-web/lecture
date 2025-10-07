2e5ff66ac2f9442678a26e67625ca7d1a04b86db
unknown <prajapatiprachi682@gmail.com>
2025-10-07 12:02:31 +0530

    updated code

--- evenodd.c
+++ evenodd.c
@@ -1,14 +1,32 @@
-#include <stdio.h>
+// #include <stdio.h>
+// int main()
+// {
+//    int a=11;
+//    int  b=21;
+//      if(a%2==0 )
+//      {
+//         printf("even");
+//       }else
+//         {
+//            printf("odd");  
+//         }
+//      }
+      
+
+     #include <stdio.h>
 int main()
 {
-   int a=11;
-   int  b=21;
-     if(a%2==0 )
-     {
-        printf("even");
-      }else
-        {
-           printf("odd");  
-        }
-     }
-      
+    int num;
+    printf("enter a number:");
+    scanf("%d",& num);
+    switch (num %2)
+    { 
+        case 1:
+        printf("even\n");
+        break;
+
+        case 2:
+        printf("odd\n");
+        break;
+    }
+}
