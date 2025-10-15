bd266f05625240163ecf34c770aeaf27f361a562
Prachi Prajapati <prajapatiprachi682@gmail.com>
2025-10-15 23:01:37 +0530

    new code

---
+++ C programming/C9.c
@@ -0,0 +1,57 @@
+// #include <stdio.h>
+// void odd_even( ){
+// //    if(a%2==0){
+// //     printf("even");
+// //    }else{
+// //     printf("oddd");
+// //    }
+// printf("hello");
+// return 10
+
+// }
+// // int sum(){
+// //     int a=5;
+// //     int b=5;
+// //     printf("%d",a+b);
+// // }
+// int main(){
+//     //  int a;
+//     //  scanf("%d",&a);
+//     // odd_even(a);
+//     // odd_even(a);
+//     // odd_even();
+//     odd_even();
+   
+//     return 0;
+//     //  printf("hello");
+
+
+// }
+
+
+    // /or(int i=0;i<=4;i++){
+    //     for(int j=0;j<=4;j++){
+    //         if(i==j|| j==(4-i)){
+    //             printf("*");
+    //         }else{
+    //             printf(" ");
+    //         }
+    //     }printf("\n");
+    // }
+
+#include <stdio.h>
+int sum(){
+    int a=5;
+    int b=10;
+    int c=a+b;
+    return c;
+    // printf("%d",a+b);
+}
+int main(){
+  int data=   sum();
+  printf("%d",data);
+
+}
+
+
+//1234  => 1+2+3+4=10=> 1+0=1
