65674eb79da647b3cf3df9e8fb51d695e70b4697
Prachi Prajapati <prajapatiprachi682@gmail.com>
2025-10-15 22:21:10 +0530

    updated code

---
+++ C programming/C3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int a = 2;
+
+    switch (a)
+    {
+        case 1:
+            printf("hello");
+            break;
+
+        case 2:
+            printf("hiii");
+            break;
+
+        default:
+            printf("kuch nhi mila");
+            break;
+    }
+}
+
+
+
+
+// int a<5;
+// printf("hello");
+// printf("hii");
+// }
