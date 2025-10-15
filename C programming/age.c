b012e55b3555a2f48354833c61ea34bdd411d79b
Prachi Prajapati <prajapatiprachi682@gmail.com>
2025-10-15 21:36:37 +0530

    add

--- C programming/age.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int age=20;
-    printf ("age=%d\n",age);
-}
--- C programming/asc.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-     int ascii = 65;
-    char ch = ascii;  // converting int to char
-    printf("ASCII value %d = character '%c'\n", ascii, ch);
-
-}
